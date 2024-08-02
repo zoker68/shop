d5338ad76b25af8beba8e38bb693e596d60890e6
Maksim <zokermm@gmail.com>
2024-08-02 16:43:22 +0200

    init

---
+++ src/Providers/ShopServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zoker68\Shop\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+
+}
