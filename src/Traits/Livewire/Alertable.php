1ca490d48d2a463ac1b99e9a34593dab61f1a960
Maksim <zokermm@gmail.com>
2024-10-25 23:56:54 +0200

    Small changes

--- src/Traits/Livewire/Alertable.php
+++ src/Traits/Livewire/Alertable.php
@@ -4,8 +4,16 @@ namespace Zoker\Shop\Traits\Livewire;
 
 trait Alertable
 {
-    public function throwAlert(string $type, string $message, int $timeout = 2): void
+    public function throwAlert(string $type, string $message, ?int $timeout = null): void
     {
+        if (! $timeout) {
+            $timeout = match ($type) {
+                'danger', 'warning' => 3,
+                'error' => 5,
+                default => 2,
+            };
+        }
+
         $this->dispatch('livewire:alert', [
             'type' => $type,
             'message' => $message,
