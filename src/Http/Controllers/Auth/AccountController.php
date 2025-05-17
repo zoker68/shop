951876c809b3102e67ebe5ac9f8d25a3eb116027
Maksim <zokermm@gmail.com>
2025-05-17 22:30:48 +0200

    Edit composer json version

--- src/Http/Controllers/Auth/AccountController.php
+++ src/Http/Controllers/Auth/AccountController.php
@@ -8,7 +8,7 @@ class AccountController extends Controller
 {
     public function __invoke()
     {
-        $orders = auth()->user()->orders()->with(['products', 'products.product'])->latest()->take(3)->get();
+        $orders = auth()->user()->orders()->with(['products', 'products.product', 'generalStatus'])->latest()->take(3)->get();
 
         return view('shop::pages.auth.account.index', compact('orders'));
     }
